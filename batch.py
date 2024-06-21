062e185609ae63b863d67df8828c10e0837540ec
Scott McElroy <scottmcelroy54@gmail.com>
2024-06-21 15:36:15 -0400

    Updating experimental data analysis documentation

--- batch.py
+++ batch.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'L6IETune0621'
+    b.batchLabel = 'L6IETune0621A'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_slurm_Expanse')
