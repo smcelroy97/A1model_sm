79a16210c9942abf6018478bc1059c5d59264383
Scott McElroy <scottmcelroy54@gmail.com>
2024-08-14 13:17:35 -0400

    'MyParamNewGabaB0814A'
    Mix of aSam's and mine

--- batch.py
+++ batch.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     # b = evolRates('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'MyParamNewGabaB0814'
+    b.batchLabel = 'MyParamNewGabaB0814A'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_sge')
--- cfg.py
+++ cfg.py
@@ -169,19 +169,19 @@ cfg.scaleDensity = 1.0  # Should be 1.0 unless need lower cell density for test 
 cfg.addConn = 1.0
 cfg.wireCortex = 1.0
 
-# cfg.EEGain = 0.75
-# cfg.EIGain = 1.5
-# cfg.IEGain = 1.5
-# cfg.IIGain = 1.0
+cfg.EEGain = 0.75
+cfg.EIGain = 1.5
+cfg.IEGain = 1.5
+cfg.IIGain = 1.0
 # cfg.L6IEGain = 1.0
 
- # My old params don't delete
-# cfg.EEGain = 1.18702850562324
-cfg.EEGain = 1.1
-cfg.EIGain = 1.6313576020869256
-# cfg.IEGain = 1.716277020224909
-cfg.IEGain = 2.06
-cfg.IIGain = 1.4102431748127964
+# My old params don't delete
+# # cfg.EEGain = 1.18702850562324
+# cfg.EEGain = 1.1
+# cfg.EIGain = 1.6313576020869256
+# # cfg.IEGain = 1.716277020224909
+# cfg.IEGain = 2.06
+# cfg.IIGain = 1.4102431748127964
 
 
 ## E/I->E/I layer weights (L1-3, L4, L5, L6)
