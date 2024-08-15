4360d0167de095a698fa708716a1643eb826c775
Scott McElroy <scottmcelroy54@gmail.com>
2024-08-15 13:01:43 -0400

    Copy fixed

--- batch.py
+++ batch.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     # b = evolRates('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'SamParams0815'
+    b.batchLabel = 'SamWavTest0815'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_sge')
