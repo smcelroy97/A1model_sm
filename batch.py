8368043b6a3a538961584e2274c72c4c5595dc12
Scott McElroy <scottmcelroy54@gmail.com>
2024-08-26 15:05:56 -0400

    plotting the conn to test differences

--- batch.py
+++ batch.py
@@ -275,8 +275,8 @@ if __name__ == '__main__':
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     # b = evolRates('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'Me_Sam0816'
+    b.batchLabel = 'Me_Sam0826'
     b.saveFolder = 'data/'+b.batchLabel
 
-    setRunCfg(b, 'hpc_slurm_Expanse')
+    setRunCfg(b, 'hpc_sge')
     b.run() # run batch
